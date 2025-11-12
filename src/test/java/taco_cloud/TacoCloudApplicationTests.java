160de0e93fc1cb53a96abc8b4b91f723d7877500
CheeseToxin <125279621+CheeseToxin@users.noreply.github.com>
2025-11-13 03:19:25 +0400

    part1.4

--- src/test/java/taco_cloud/TacoCloudApplicationTests.java
+++ src/test/java/taco_cloud/TacoCloudApplicationTests.java
@@ -1,11 +1,16 @@
 package taco_cloud;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 class TacoCloudApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
